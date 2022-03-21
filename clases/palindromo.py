3f39b30d38961dc0dcbfb17ab9b25215a303803d
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-03-21 17:45:15 +0000

    definimos la variable palabra

--- clases/palindromo.py
+++ clases/palindromo.py
@@ -2,4 +2,6 @@ class palindromo():
   def __init__(self,palabra):
     self.palabra=palabra
   def verificacion(self):
+    palabra = input('Introduzca la palabra que desea verificar si es palindroma')
+    
     
