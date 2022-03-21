acfb00fca83e284580545782be861dab6a0aee72
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-03-21 17:46:11 +0000

    creo un condicional que nos ayudará a verificarlo

--- clases/logger.py
+++ clases/logger.py
@@ -1,22 +1,16 @@
 class Logger:
     def __init__(self):
         self.log_file = open("log.txt", "w")
-        # Initialización del contador de logs
         self.log_count = 0
-        # Escritura de la primera línea
         self.log_file.write("--Inicio del log--\n")
 
 
     def __del__(self):
-        # Destrucción de la instancia: escribimos la última línea del archivo
         self.log_file.write("--Fin del log: {} log(s)--\n".format(self.log_count))
-        # Cierre limpio del archivo
         self.log_file.close()
 
     def log(self, message):
-        # Escritura del mensaje que se pasa como argumento
         self.log_file.write("{}\n".format(message))
-        # Incremento del contador de logs
         self.log_count += 1
 
 
--- clases/palindromo.py
+++ clases/palindromo.py
@@ -3,5 +3,9 @@ class palindromo():
     self.palabra=palabra
   def verificacion(self):
     palabra = input('Introduzca la palabra que desea verificar si es palindroma')
+    if palabra == ''.join(reversed(palabra)):
+      print('True')
+    else:
+      print('False')
     
     
