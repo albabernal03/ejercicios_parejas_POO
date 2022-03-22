61a580d53ce94077753668c0d34bbb7484837c26
Carmen <91721886+carmenm02@users.noreply.github.com>
2022-03-22 12:01:36 +0000

    he creado el main del tercer ejercicio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from clases.palindromo import palindromo
 from clases.palindromo_2 import palindromo_2
+from clases.puzzle import puzzle
 
 
 
@@ -9,4 +10,16 @@ if __name__ == "__main__":
 
 if __name__ == "__main__":
   print(palindromo_2.verificacion('palabra'))
+
+if __name__ == "__main__":
+  a = A 
+  y = a.z 
+  print(y(a)) 
+  aa = a() 
+  print(aa is a()) 
+  z = aa.y 
+  print(z(())) 
+  print(a().y((a,))) 
+  print(A.y(aa, (a,z))) 
+  print(aa.y((z,1,'z'))) 
     
