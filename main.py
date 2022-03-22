4561b911542a9fc38789796398a7e9556366f6e0
Carmen <91721886+carmenm02@users.noreply.github.com>
2022-03-22 11:56:28 +0000

    importamos main del segundo ejercicio

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from clases.palindromo import palindromo
-
+from clases.palindromo_2 import palindromo_2
 
 
 
 
 if __name__ == "__main__":
   print(palindromo.verificacion('palabra'))
+
+if __name__ == "__main__":
+  print(palindromo_2.verificacion('palabra'))
     
